71d35847290b4bf9835b0124a9d027ebb342d380
Amaury <amauryleclerc@hotmail.fr>
2016-09-03 16:55:03 +0200

    refacto search

--- public/app/yt/ytSearchCtrl.js
+++ public/app/yt/ytSearchCtrl.js
@@ -1,23 +1,33 @@
 'use strict';
 
-app.controller('YtSearchCtrl', [ '$scope', 'Yt', 'Player', function($scope, Yt, Player) {
+app.controller('YtSearchCtrl', ['$scope', 'Yt', 'Player', function ($scope, Yt, Player) {
 	this.query = "";
 	this.videos = [];
-	var that  = this;
-	this.search = function() {
+	var that = this;
+	this.search = function () {
 		console.log("search");
 		//gapi.login().then(callbackCreator(this.query, this.videos));
-		 Yt.search({ part: 'snippet', q: that.query, key:"AIzaSyBuOzo_bwsr4JP0U993vNhJ9mKH6dfH9Ks"},function(data){
-			 that.videos = data.items.filter(function(video){
+		var search = {
+			part: 'snippet',
+			q: that.query,
+			key: "AIzaSyBuOzo_bwsr4JP0U993vNhJ9mKH6dfH9Ks",
+			//videoEmbeddable:"true",
+			type:"video",
+		//	videoSyndicated:"true",
+			videoLicense:"creativeCommon"
+		
+		}
+		Yt.search(search, function (data) {
+			that.videos = data.items.filter(function (video) {
 				return video.id.kind == "youtube#video";
-			 } );
-			 console.log(	 that.videos);
+			});
+			console.log(that.videos);
 		});
 	}
-	this.add = function(video){
+	this.add = function (video) {
 		console.log("add");
-		var music = {id:video.id.videoId, title:video.snippet.title, album:{name:video.snippet.channelTitle}, artist:{name:''}};
+		var music = { id: video.id.videoId, title: video.snippet.title, album: { name: video.snippet.channelTitle }, artist: { name: '' } };
 		Player.add(music);
 	}
 
-} ]);
+}]);
