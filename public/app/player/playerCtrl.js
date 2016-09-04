4916cff7ae9825900d87ba7e63a6b715ec9d0085
Amaury <amauryleclerc@hotmail.fr>
2016-09-04 11:24:37 +0200

    affichage player si music présente

--- public/app/player/playerCtrl.js
+++ public/app/player/playerCtrl.js
@@ -3,7 +3,7 @@ angular.module('raspMusicApp').controller('PlayerCtrl', ['Player', 'PlayerServic
 	this.music = null;
 	this.state = null;
 	this.time = null;
-	PlayerService.getCurrent((music)=>{
+	PlayerService.getCurrent((music) => {
 		this.music = music;
 	})
 	this.play = function () {
@@ -27,6 +27,11 @@ angular.module('raspMusicApp').controller('PlayerCtrl', ['Player', 'PlayerServic
 
 	PlayerService.onPlay((music) => {
 		this.music = music;
+	});
+	PlayerService.onAdd((music) => {
+		if (this.music == null) {
+			this.music = music;
+		}
 	});
 	PlayerService.onStateChange((state) => {
 		this.state = state;
